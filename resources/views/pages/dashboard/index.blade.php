5b9de8e135fdbb40d81bc64393eefbcf3bccbe83
ahmad_wahyu_ism <ahmad.wahyu@integrasolution.id>
2025-06-25 11:02:42 +0700

    Initial commit

---
+++ resources/views/pages/dashboard/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Selamat Datang di Sistem Pelaporan Bencana - SPB</h1>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+@endsection
