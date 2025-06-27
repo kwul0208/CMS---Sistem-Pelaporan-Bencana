65b8788c11a6d063a6512febf2b89c944d8293d5
ahmad_wahyu_ism <ahmad.wahyu@integrasolution.id>
2025-06-27 17:30:29 +0700

    API Auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthApiController;
 use App\Http\Controllers\DataSaluranController;
 use App\Http\Controllers\LaporanPekerjaanRutinController;
 use App\Http\Controllers\TanggapDaruratBencanaController;
@@ -18,32 +19,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/login', [AuthApiController::class, 'login']);
 
-// Profile
-Route::get('profile/{id}', [UserController::class, 'getAPIProfile']);
-Route::post('profile/update/{id}', [UserController::class, 'updateAPIProfile']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthApiController::class, 'logout']);
+    
+    // Profile
+    Route::get('profile/{id}', [UserController::class, 'getAPIProfile']);
+    Route::post('profile/update/{id}', [UserController::class, 'updateAPIProfile']);
 
 
-// Tanggap Darurat Bencana
-Route::get('tanggap-darurat-bencana', [TanggapDaruratBencanaController::class, 'index']);
-Route::get('tanggap-darurat-bencana/show/{id}', [TanggapDaruratBencanaController::class, 'show']);
-Route::post('tanggap-darurat-bencana/store', [TanggapDaruratBencanaController::class, 'store']);
-Route::post('tanggap-darurat-bencana/update', [TanggapDaruratBencanaController::class, 'update']);
-Route::delete('tanggap-darurat-bencana/delete/{id}', [TanggapDaruratBencanaController::class, 'delete']);
+    // Tanggap Darurat Bencana
+    Route::get('tanggap-darurat-bencana', [TanggapDaruratBencanaController::class, 'index']);
+    Route::get('tanggap-darurat-bencana/show/{id}', [TanggapDaruratBencanaController::class, 'show']);
+    Route::post('tanggap-darurat-bencana/store', [TanggapDaruratBencanaController::class, 'store']);
+    Route::post('tanggap-darurat-bencana/update', [TanggapDaruratBencanaController::class, 'update']);
+    Route::delete('tanggap-darurat-bencana/delete/{id}', [TanggapDaruratBencanaController::class, 'delete']);
 
-// Laporan Pekerjaan Rutin
-Route::get('laporan-pekerjaan-rutin', [LaporanPekerjaanRutinController::class, 'index']);
-Route::get('laporan-pekerjaan-rutin/show/{id}', [LaporanPekerjaanRutinController::class, 'show']);
-Route::post('laporan-pekerjaan-rutin/store', [LaporanPekerjaanRutinController::class, 'store']);
-Route::post('laporan-pekerjaan-rutin/update', [LaporanPekerjaanRutinController::class, 'update']);
-Route::delete('laporan-pekerjaan-rutin/delete/{id}', [LaporanPekerjaanRutinController::class, 'delete']);
+    // Laporan Pekerjaan Rutin
+    Route::get('laporan-pekerjaan-rutin', [LaporanPekerjaanRutinController::class, 'index']);
+    Route::get('laporan-pekerjaan-rutin/show/{id}', [LaporanPekerjaanRutinController::class, 'show']);
+    Route::post('laporan-pekerjaan-rutin/store', [LaporanPekerjaanRutinController::class, 'store']);
+    Route::post('laporan-pekerjaan-rutin/update', [LaporanPekerjaanRutinController::class, 'update']);
+    Route::delete('laporan-pekerjaan-rutin/delete/{id}', [LaporanPekerjaanRutinController::class, 'delete']);
 
-// Data Saluran
-Route::get('data-saluran', [DataSaluranController::class, 'index']);
-Route::get('data-saluran/show/{id}', [DataSaluranController::class, 'show']);
-Route::post('data-saluran/store', [DataSaluranController::class, 'store']);
-Route::post('data-saluran/update', [DataSaluranController::class, 'update']);
-Route::delete('data-saluran/delete/{id}', [DataSaluranController::class, 'delete']);
+    // Data Saluran
+    Route::get('data-saluran', [DataSaluranController::class, 'index']);
+    Route::get('data-saluran/show/{id}', [DataSaluranController::class, 'show']);
+    Route::post('data-saluran/store', [DataSaluranController::class, 'store']);
+    Route::post('data-saluran/update', [DataSaluranController::class, 'update']);
+    Route::delete('data-saluran/delete/{id}', [DataSaluranController::class, 'delete']);
+});
