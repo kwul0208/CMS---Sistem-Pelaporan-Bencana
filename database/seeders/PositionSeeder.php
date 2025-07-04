f92a3699593e94d5f08b832498e67d48335b42f1
kwul0208 <kwul0208@gmail.com>
2025-07-04 23:13:38 +0700

    fix org position

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -9,31 +9,93 @@ class PositionSeeder extends Seeder
 {
     public function run(): void
     {
-        // Struktur Induk
-        $head = Position::create(['name' => 'Kepala UPTD SDA Wilayah V', 'parent_id' => 0]);
-        $kasubtu = Position::create(['name' => 'Kasub TU UPTD SDA Wilayah V', 'parent_id' => $head->id]);
+        // Level 1 - Top Leadership
+        $kepala = Position::create([
+            'name' => 'Kepala UPTD SDA Wilayah V',
+            'parent_id' => null
+        ]);
 
-        // Sub-divisi
-        $peng_admin = Position::create(['name' => 'Peng. ADM UMUM', 'parent_id' => $kasubtu->id]);
-        $teknisi_air = Position::create(['name' => 'Teknisi Air', 'parent_id' => $head->id]);
+        // Level 2 - Direct Reports
+        $kasub_tu = Position::create([
+            'name' => 'Kasub TU UPTD SDA Wilayah V',
+            'parent_id' => $kepala->id
+        ]);
 
-        $tim_admin = Position::create(['name' => 'Tim Administrasi', 'parent_id' => $kasubtu->id]);
-        $tim_teknis = Position::create(['name' => 'Tim Teknis', 'parent_id' => $head->id]);
-        $humas_media = Position::create(['name' => 'Humas & Media', 'parent_id' => $kasubtu->id]);
+        $peng_admin = Position::create([
+            'name' => 'Peng. ADM UMUM',
+            'parent_id' => $kepala->id
+        ]);
 
-        // Koordinator & Wilayah
-        $wilayah = [
-            'Wilayah Kec. Sepatan Timur',
-            'Wilayah Kec. Sepatan',
-            'Wilayah Kec. Sukadiri',
-            'Mauk Bagian Timur',
-            'Wilayah Kecamatan Mauk',
-            'Mauk Bagian Barat',
+        $teknisi_air = Position::create([
+            'name' => 'Teknisi Air',
+            'parent_id' => $kepala->id
+        ]);
+
+        // Level 3 - Teams
+        $tim_admin = Position::create([
+            'name' => 'Tim Administrasi',
+            'parent_id' => $kasub_tu->id
+        ]);
+
+        $humas_media = Position::create([
+            'name' => 'Humas & Media',
+            'parent_id' => $kasub_tu->id
+        ]);
+
+        $tim_teknis = Position::create([
+            'name' => 'Tim Teknis',
+            'parent_id' => $kepala->id
+        ]);
+
+        // Level 4 - Regional Coordinators
+        $koordinator_sepatan_timur = Position::create([
+            'name' => 'Koordinator Lapangan Wilayah Kec. Sepatan Timur',
+            'parent_id' => $kepala->id
+        ]);
+
+        $koordinator_sepatan = Position::create([
+            'name' => 'Koordinator Lapangan Wilayah Kec. Sepatan',
+            'parent_id' => $kepala->id
+        ]);
+
+        $koordinator_sukadiri = Position::create([
+            'name' => 'Koordinator Lapangan Wilayah Kec. Sukadiri',
+            'parent_id' => $kepala->id
+        ]);
+
+        $koordinator_mauk_timur = Position::create([
+            'name' => 'Koordinator Lapangan Mauk Bagian Timur',
+            'parent_id' => $kepala->id
+        ]);
+
+        $koordinator_mauk = Position::create([
+            'name' => 'Koordinator Lapangan Wilayah Kecamatan Mauk',
+            'parent_id' => $kepala->id
+        ]);
+
+        // Level 5 - Field Staff Members
+        $field_positions = [
+            // Sepatan Timur
+            ['name' => 'Anggota Wilayah Kec. Sepatan Timur', 'parent' => $koordinator_sepatan_timur->id],
+            
+            // Sepatan
+            ['name' => 'Anggota Wilayah Kec. Sepatan', 'parent' => $koordinator_sepatan->id],
+            
+            // Sukadiri
+            ['name' => 'Anggota Wilayah Kec. Sukadiri', 'parent' => $koordinator_sukadiri->id],
+            
+            // Mauk Timur
+            ['name' => 'Anggota Mauk Bagian Timur', 'parent' => $koordinator_mauk_timur->id],
+            
+            // Mauk
+            ['name' => 'Anggota Wilayah Kecamatan Mauk', 'parent' => $koordinator_mauk->id],
         ];
 
-        foreach ($wilayah as $w) {
-            Position::create(['name' => "Koordinator Lapangan $w", 'parent_id' => $head->id]);
-            Position::create(['name' => "Anggota $w", 'parent_id' => $head->id]);
+        foreach ($field_positions as $position) {
+            Position::create([
+                'name' => $position['name'],
+                'parent_id' => $position['parent']
+            ]);
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,42 +12,59 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $users = [
+            ['name' => 'Admin', 'position' => ''],
             ['name' => 'Aefine Rumedi, S.T., M.IP', 'position' => 'Kepala UPTD SDA Wilayah V'],
             ['name' => 'Mia Fitita Devi, S.T', 'position' => 'Kasub TU UPTD SDA Wilayah V'],
             ['name' => 'Heldi Apriadi', 'position' => 'Peng. ADM UMUM'],
             ['name' => 'Nana Suryana', 'position' => 'Teknisi Air'],
+            
+            // Tim Administrasi
             ['name' => 'Niar Arfidaniah, S.T', 'position' => 'Tim Administrasi'],
             ['name' => 'Siti Rosiah', 'position' => 'Tim Administrasi'],
             ['name' => 'Nurshea, S.Kom', 'position' => 'Tim Administrasi'],
             ['name' => 'Misja', 'position' => 'Tim Administrasi'],
             ['name' => 'Dedi Suryadi', 'position' => 'Tim Administrasi'],
+            
+            // Humas & Media
             ['name' => 'Jemmy Susilo, S.E', 'position' => 'Humas & Media'],
             ['name' => 'Aldi Setiawan, S.H', 'position' => 'Humas & Media'],
             ['name' => 'Teguh Akbar', 'position' => 'Humas & Media'],
             ['name' => 'Ghassan Austin B., S.I.Kom', 'position' => 'Humas & Media'],
+            
+            // Tim Teknis
             ['name' => 'Asep Mulyadi, S.IP', 'position' => 'Tim Teknis'],
             ['name' => 'Purnomo Satria, N.S., Kom', 'position' => 'Tim Teknis'],
             ['name' => 'Dede Sandi', 'position' => 'Tim Teknis'],
             ['name' => 'Nurul Yustika, S.T', 'position' => 'Tim Teknis'],
             ['name' => 'Darto', 'position' => 'Tim Teknis'],
+            
+            // Koordinator Lapangan Wilayah Kec. Sepatan Timur
             ['name' => 'Mangsur', 'position' => 'Koordinator Lapangan Wilayah Kec. Sepatan Timur'],
             ['name' => 'Amid Dayut', 'position' => 'Anggota Wilayah Kec. Sepatan Timur'],
             ['name' => 'Hendrik', 'position' => 'Anggota Wilayah Kec. Sepatan Timur'],
             ['name' => 'Resin', 'position' => 'Anggota Wilayah Kec. Sepatan Timur'],
+            
+            // Koordinator Lapangan Wilayah Kec. Sepatan
             ['name' => 'Aswi Suprianto', 'position' => 'Koordinator Lapangan Wilayah Kec. Sepatan'],
             ['name' => 'Rusdi', 'position' => 'Anggota Wilayah Kec. Sepatan'],
             ['name' => 'Sudin', 'position' => 'Anggota Wilayah Kec. Sepatan'],
             ['name' => 'M. Arif Soleh', 'position' => 'Anggota Wilayah Kec. Sepatan'],
+            
+            // Koordinator Lapangan Wilayah Kec. Sukadiri
             ['name' => 'Dulhadi', 'position' => 'Koordinator Lapangan Wilayah Kec. Sukadiri'],
             ['name' => 'Sartamin', 'position' => 'Anggota Wilayah Kec. Sukadiri'],
             ['name' => 'Suparman', 'position' => 'Anggota Wilayah Kec. Sukadiri'],
             ['name' => 'Amsin', 'position' => 'Anggota Wilayah Kec. Sukadiri'],
+            
+            // Koordinator Lapangan Mauk Bagian Timur
             ['name' => 'Basri', 'position' => 'Koordinator Lapangan Mauk Bagian Timur'],
             ['name' => 'Abdurohman', 'position' => 'Anggota Mauk Bagian Timur'],
             ['name' => 'Nurhadi', 'position' => 'Anggota Mauk Bagian Timur'],
             ['name' => 'Badri', 'position' => 'Anggota Mauk Bagian Timur'],
             ['name' => 'Yahya', 'position' => 'Anggota Mauk Bagian Timur'],
             ['name' => 'Andri Salosa', 'position' => 'Anggota Mauk Bagian Timur'],
+            
+            // Koordinator Lapangan Wilayah Kecamatan Mauk
             ['name' => 'Aswan', 'position' => 'Koordinator Lapangan Wilayah Kecamatan Mauk'],
             ['name' => 'Thomas', 'position' => 'Anggota Wilayah Kecamatan Mauk'],
             ['name' => 'Abdul Mazid', 'position' => 'Anggota Wilayah Kecamatan Mauk'],
@@ -57,13 +74,18 @@ class UserSeeder extends Seeder
         ];
 
         foreach ($users as $index => $user) {
-            $position = Position::where('name', $user['position'])->first();
+            // Find position, handle empty position for Admin
+            $position = null;
+            if (!empty($user['position'])) {
+                $position = Position::where('name', $user['position'])->first();
+            }
 
             User::create([
                 'name' => $user['name'],
-                'email' => 'user' . ($index + 1) . '@example.com',
+                'email' => $user['name'] == 'Admin' ? 'admin@example.com' : 'user' . ($index + 1) . '@example.com',
                 'password' => Hash::make('password'),
-                'position_id' => $position?->id ?? 1, // fallback to 1 if not found
+                'position_id' => $position?->id,
+                'role' => $user['name'] == 'Admin' ? 'admin' : 'user'
             ]);
         }
     }
